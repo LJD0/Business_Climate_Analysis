62e8436378157cfa87423a840b914606c8e14b6b
OAO-MadHatter <larsenj2064@gmail.com>
2022-11-02 21:07:10 -0700

    9.5 final commit
    app.py complete with all functions

--- app.py
+++ app.py
@@ -24,34 +24,66 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
 
-    return( """
+    return """
     Welcome to the Climate Analysis API!
     Available Routes:
     /api/v1.0/precipitation
     /api/v1.0/stations
     /api/v1.0/tobs
     /api/v1.0/temp/start/end
-    """)
+    """
 
 
 @app.route("/api/v1.0/precipitation")
-
-def precipitation ():
-    return
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = (
+        session.query(Measurement.date, Measurement.prcp)
+        .filter(Measurement.date >= prev_year)
+        .all()
+    )
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
 
 
 @app.route("/api/v1.0/stations")
-
-def stations ():
-    return
+def stations():
+    station = session.query(Station.station).all()
+    stations = list(np.ravel(station))
+    return jsonify(stations=stations)
 
 
 @app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = (
+        session.query(Measurement.tobs)
+        .filter(Measurement.station == "USC00519281")
+        .filter(Measurement.date >= prev_year)
+        .all()
+    )
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
 
-def stats():
-    return
 
-@app.route("/api/v1.0/temp/start/end")
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel = [
+        func.min(Measurement.tobs),
+        func.avg(Measurement.tobs),
+        func.max(Measurement.tobs),
+    ]
+    results = (
+        session.query(*sel)
+        .filter(Measurement.date >= start)
+        .filter(Measurement.date <= end)
+        .all()
+    )
+    if not end:
+        results = session.query(*sel).filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
 
-def monthly_temps ():
-    
+    temps = list(np.ravel(results))
+    return jsonify(temps)
